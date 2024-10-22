7e8df7a5cc2f8a539a143c4522f8098d33ec38ba
Daniel Fernandes <danieljosephfernandes@gmail.com>
2024-10-22 15:03:37 +0100

    Add test_sends_tracer_headers, fixtures

--- tests/unit_tests/test_tracing.py
+++ tests/unit_tests/test_tracing.py
@@ -1,9 +1,47 @@
-from unittest.mock import Mock
+from unittest.mock import ANY, Mock
 
 import pytest
+from observability_utils.tracing import get_tracer, setup_tracing  # type: ignore
+from opentelemetry.trace import Tracer
 from stomp.connect import StompConnection11 as Connection  # type: ignore
+
+from bluesky_stomp.messaging import StompClient
+from bluesky_stomp.models import MessageQueue
+
+setup_tracing("test_tracing", False)
 
 
 @pytest.fixture
 def mock_connection() -> Mock:
     return Mock(spec=Connection)
+
+
+@pytest.fixture
+def client(mock_connection: Mock) -> StompClient:
+    return StompClient(conn=mock_connection)
+
+
+# Depends on client to ensure fixtures are executed in correct order
+@pytest.fixture
+def mock_listener(mock_connection: Mock, client: StompClient) -> Mock:
+    return mock_connection.set_listener.mock_calls[0].args[1]
+
+
+@pytest.fixture
+def tracer() -> Tracer:
+    return get_tracer("fixture")  # type: ignore
+
+
+def test_sends_tracer_headers(
+    mock_connection: Mock,
+    client: StompClient,
+):
+    client.send(MessageQueue(name="misc"), "misc")
+    mock_connection.send.assert_called_once_with(
+        headers={
+            "JMSType": "TextMessage",
+            "traceparent": ANY,
+        },
+        body=ANY,
+        destination=ANY,
+    )
